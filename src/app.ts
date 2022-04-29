6745356ba16b2981d906bca724157954ae81645a
LukeMcCann <developer-lukemccann@outlook.com>
2022-04-29 20:12:52 +0100

    Add validation function

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,49 @@ function AutoBind(_target: any, _methodName: string, descriptor: PropertyDescrip
     return adjDescriptor;
 };
 
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+      isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (
+      validatableInput.minLength != null &&
+      typeof validatableInput.value === 'string'
+    ) {
+      isValid =
+        isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (
+      validatableInput.maxLength != null &&
+      typeof validatableInput.value === 'string'
+    ) {
+      isValid =
+        isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (
+      validatableInput.min != null &&
+      typeof validatableInput.value === 'number'
+    ) {
+      isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (
+      validatableInput.max != null &&
+      typeof validatableInput.value === 'number'
+    ) {
+      isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+  }
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -36,17 +79,64 @@ class ProjectInput {
         this.attach();
     }
 
-    @AutoBind
-    private submitHandler(event: Event) {
-        event.preventDefault();
-        console.log(this.titleInputElement.value);
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionTextAreaElement.value = '';
+        this.peopleInputElement.value = '';
+      }
+
+    private gatherUserInput() : [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionTextAreaElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+            minLength: 1,
+            maxLength: 15,
+        };
+
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 1,
+        };
+
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5,
+        };
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        }
+        return [enteredTitle, enteredDescription, +enteredPeople];
     }
 
-    private configure() {
+    @AutoBind
+    private submitHandler(event: Event) : void {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    }
+
+    private configure() : void {
         this.formElement.addEventListener('submit', this.submitHandler);
     }
 
-    private attach() {
+    private attach() : void {
         this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
     }
 }
