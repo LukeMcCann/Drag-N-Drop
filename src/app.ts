ba872423096596132804870cb86bea20917e1f7f
LukeMcCann <developer-lukemccann@outlook.com>
2022-04-29 22:01:57 +0100

    Fix typing

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 class ProjectState {
     private listeners: any[] = [];
-    private projects : any[] = [];
+    private projects : Project[] = [];
     private static instance : ProjectState;
 
     private constructor() {}
@@ -17,12 +17,13 @@ class ProjectState {
     }
 
     public addProject(title: string, description: string, numPeople: number) : void {
-        const newProject = {
-            id: Math.random.toString(),
-            title: title,
-            description: description,
-            people: numPeople,
-        };
+        const newProject = new Project(
+            Math.random().toString(),
+            title,
+            description,
+            numPeople,
+            ProjectStatus.Active,
+        );
         this.projects.push(newProject);
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
@@ -175,15 +176,25 @@ class ProjectInput {
     }
 }
 
-class ProjectItem {
+enum ProjectStatus { Active, Finished };
 
+class Project {
+    constructor (
+        public id: string,
+        public title: string,
+        public description: string,
+        public people: number,
+        public status: ProjectStatus,
+    ) {
+
+    }
 }
 
 class ProjectList {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
-    assignedProjects: any[];
+    assignedProjects: Project[];
 
     constructor(
         private status: 'active' | 'finished'
@@ -196,7 +207,7 @@ class ProjectList {
 
         (this.element = importedNode.firstElementChild as HTMLElement).id = `${status}-projects`;
 
-        globalState.addListener((projects: any[]) => {
+        globalState.addListener((projects: Project[]) => {
             this.assignedProjects = projects;
             this.renderProjects();
         });
