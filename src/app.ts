6c3c8db1e1e549f210463159fc11825931d11a8d
LukeMcCann <developer-lukemccann@outlook.com>
2022-04-29 19:26:28 +0100

    Add AutoBind

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
+function AutoBind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFunction = originalMethod.bind(this);
+            return boundFunction;
+        }
+    };
+    return adjDescriptor;
+};
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
-    concreteFormElement: HTMLFormElement;
+    formElement: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionTextAreaElement: HTMLTextAreaElement;
+    peopleInputElement: HTMLInputElement;
+
 
     constructor() {
         this.templateElement = document.querySelector('#project-input') as HTMLTemplateElement;
@@ -9,12 +26,28 @@ class ProjectInput {
 
         const importedFormNode = document.importNode(this.templateElement.content, true);
 
-        this.concreteFormElement = importedFormNode.firstElementChild as HTMLFormElement;
+        (this.formElement = importedFormNode.firstElementChild as HTMLFormElement).id = 'user-input';
+
+        this.titleInputElement = this.formElement.querySelector('#title') as HTMLInputElement;
+        this.descriptionTextAreaElement = this.formElement.querySelector('#description') as HTMLTextAreaElement;
+        this.peopleInputElement = this.formElement.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
     }
 
+    @AutoBind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.formElement.addEventListener('submit', this.submitHandler);
+    }
+
     private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.concreteFormElement);
+        this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
     }
 }
 
